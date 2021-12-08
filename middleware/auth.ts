55c3439da2e300af0d251eabceb0e0ffaa547fef
vhphong <vhphong@gmail.com>
2021-12-07 21:40:51 -0500

    1st commit

---
+++ middleware/auth.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.header('Authorization');
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.sendStatus(401);
+
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        console.log('decoded');
+        console.log(decoded);
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(403);
+    }
+}
+
+module.exports = verifyToken;
